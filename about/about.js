395921bb69536cb5582de72ca3455957b350868d
Leejihi <ttoksunyi@naver.com>
2023-07-13 23:05:02 +0900

    어바웃 페이지 정리, 커리어 영역 고용노동부 이미지 변경. README.md 파일에 기록 작성

--- about/about.js
+++ about/about.js
@@ -2,28 +2,29 @@
 
 // 상단 nav 레이어 팝업 열기
 $(function () {
-  $('.flex_wrap').on('click', () => {
-    $('.nav').toggle();
+  $(".flex_wrap").on("click", () => {
+    $(".nav").toggle();
   });
 });
 
 // 상단 nav 컨텍 팝업 열기
 $(function () {
-  $('.sub-btn').on('click', () => {
-    $('.sub-popup').show();
+  $(".sub-btn").on("click", () => {
+    $(".sub-popup").show();
   });
-  $('.close-btn').on('click', () => {
-    $('.sub-popup').hide();
+  $(".close-btn").on("click", () => {
+    $(".sub-popup").hide();
   });
 });
 
 // 버튼클릭시 부드러운 이동
-$('.up-btn').click(function () {
-  $('html, body').animate({ scrollTop: 0 }, 500);
+$(".up-btn").click(function () {
+  $("html, body").animate({ scrollTop: 0 }, 500);
   return false;
 });
 
 /*  dance-me */
+/*
 gsap.registerPlugin(ScrollTrigger);
 let speed = 100;
 
@@ -37,3 +38,4 @@ ScrollTrigger.create({
 });
 
 scene1.to('.dance-me', { y: 2 * speed, x: 1 * speed }, 0.03);
+*/
