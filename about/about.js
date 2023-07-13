ec2e6dfd82e38b9af5ea8d422f1b6da68c45185d
Leejihi <ttoksunyi@naver.com>
2023-07-13 17:16:27 +0900

    about 페이지 수정 및 skill 폰트 사이즈 변경

--- about/about.js
+++ about/about.js
@@ -22,3 +22,18 @@ $('.up-btn').click(function () {
   $('html, body').animate({ scrollTop: 0 }, 500);
   return false;
 });
+
+/*  dance-me */
+gsap.registerPlugin(ScrollTrigger);
+let speed = 100;
+
+let scene1 = gsap.timeline();
+ScrollTrigger.create({
+  animation: scene1,
+  trigger: '.scrollElement',
+  start: 'top top',
+  end: '45% 80%',
+  scrub: 3,
+});
+
+scene1.to('.dance-me', { y: 2 * speed, x: 1 * speed }, 0.03);
