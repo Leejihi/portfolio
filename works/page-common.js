3e4f5bb82de6ff34784a2f8a8374fc46d88ed4fe
Leejihi <ttoksunyi@naver.com>
2023-07-17 18:02:28 +0900

    works 폴더 생성 및 이미지 소스 저장, html 코드작성

---
+++ works/page-common.js
@@ -0,0 +1,16 @@
+// 상단 nav 레이어 팝업 열기
+$(function () {
+  $('.flex_wrap').on('click', () => {
+    $('.nav').toggle();
+  });
+});
+
+// 상단 nav 컨텍 팝업 열기
+$(function () {
+  $('.sub-btn').on('click', () => {
+    $('.sub-popup').show();
+  });
+  $('.close-btn').on('click', () => {
+    $('.sub-popup').hide();
+  });
+});
