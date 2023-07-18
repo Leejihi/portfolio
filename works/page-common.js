1f9cc258daae34a4d5633ab5c52e7570a2dc2189
Leejihi <ttoksunyi@naver.com>
2023-07-18 11:26:56 +0900

    설명페이지 css 수정, js 스크롤 이벤트 추가

--- works/page-common.js
+++ works/page-common.js
@@ -14,3 +14,46 @@ $(function () {
     $('.sub-popup').hide();
   });
 });
+
+// svg 스크롤 이벤트
+
+let scrolled = () => {
+  let centerPath = document.querySelector('.line-center path');
+  let path = centerPath.getTotalLength();
+
+  centerPath.style.strokeDasharray = path;
+  centerPath.style.strokeDashoffset = path;
+
+  let windScrollTop = document.documentElement.scrollTop;
+  let scrollHt = document.documentElement.scrollHeight;
+  let clientHt = document.documentElement.clientHeight;
+
+  let total = (windScrollTop / (scrollHt - clientHt)) * 6;
+
+  let action = path * total;
+
+  centerPath.style.strokeDashoffset = path - action;
+};
+
+addEventListener('scroll', scrolled);
+
+// 솔루션 영역 도착시, 밑줄 발생
+$(window).on('scroll', () => {
+  let underHt = $('.ox-box').offset().top - 500;
+
+  if (scrollY > underHt) {
+    $('.line-pro').addClass('start');
+  } else {
+    $('.line-pro').removeClass('start');
+  }
+});
+
+$(window).on('scroll', () => {
+  let underHt = $('.ox-box').offset().top - 500;
+
+  if (scrollY > underHt) {
+    $('.line-sol').addClass('start');
+  } else {
+    $('.line-sol').removeClass('start');
+  }
+});
