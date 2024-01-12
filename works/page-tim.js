e8e92e4b538edb07ad6640c4083675d691d3e180
Leejihi <ttoksunyi@naver.com>
2024-01-12 19:16:44 +0900

    works 팀워커 설명페이지 html, css 기술서 작업 진행중행

--- works/page-tim.js
+++ works/page-tim.js
@@ -7,27 +7,6 @@ $(document).ready(function () {
   });
 });
 
-// 스크립트 영역 꽃svg 이벤트
-const mintPath = document.querySelector(".mint-line path");
-const path01 = mintPath.getTotalLength();
-
-mintPath.style.strokeDasharray = path01;
-mintPath.style.strokeDashoffset = path01;
-
-let scrolled02 = () => {
-  const windScrollTop = document.documentElement.scrollTop;
-  const scrollHt = document.documentElement.scrollHeight;
-  const clientHt = document.documentElement.clientHeight;
-
-  let total = (windScrollTop / (scrollHt - clientHt)) * 8.5;
-
-  let action = path01 * total;
-
-  mintPath.style.strokeDashoffset = path01 - action;
-};
-
-addEventListener("scroll", scrolled02);
-
 // 솔루션 영역 도착시, 밑줄 발생
 $(window).on("scroll", () => {
   let underHt = $(".direction").offset().top - 500;
